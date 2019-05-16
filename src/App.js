39e12dbd52b831a86be5d415345665358eaa6470
Sashi Kodi <sashi.kodi@gmail.com>
2019-05-16 19:18:01 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,134 @@
+import React, { Component } from 'react';
+import './App.css';
+import logo from './logo.svg';
+
+/*
+Use React and the data below to display a list of users alongside their favorite movies.
+
+For detailed instructions, refer to instructions.md.
+*/
+
+const profiles = [
+  {
+    id: 1,
+    userID: '1',
+    favoriteMovieID: '1',
+  },
+  {
+    id: 2,
+    userID: '2',
+    favoriteMovieID: '1',
+  },
+  {
+    id: 3,
+    userID: '4',
+    favoriteMovieID: '5',
+  },
+  {
+    id: 4,
+    userID: '5',
+    favoriteMovieID: '2',
+  },
+  {
+    id: 5,
+    userID: '3',
+    favoriteMovieID: '5',
+  },
+  {
+    id: 6,
+    userID: '6',
+    favoriteMovieID: '4',
+  },
+];
+
+const users = {
+  1: {
+    id: 1,
+    name: 'Jane Cruz',
+    userName: 'coder',
+  },
+  2: {
+    id: 2,
+    name: 'Matthew Johnson',
+    userName: 'mpage',
+  },
+  3: {
+    id: 3,
+    name: 'Autumn Green',
+    userName: 'user123',
+  },
+  4: {
+    id: 4,
+    name: 'John Doe',
+    userName: 'user123',
+  },
+  5: {
+    id: 5,
+    name: 'Lauren Carlson',
+    userName: 'user123',
+  },
+  6: {
+    id: 6,
+    name: 'Nicholas Lain',
+    userName: 'user123',
+  },
+};
+
+const movies = {
+  1: {
+    id: 1,
+    name: 'Planet Earth 1',
+  },
+  2: {
+    id: 2,
+    name: 'Selma',
+  },
+  3: {
+    id: 3,
+    name: 'Million Dollar Baby',
+  },
+  4: {
+    id: 4,
+    name: 'Forrest Gump',
+  },
+  5: {
+    id: 5,
+    name: 'Get Out',
+  },
+};
+
+class App extends Component {
+   getFavouriteMove(id){
+       
+         let prof= profiles.filter(profile=> profile.userID == id);
+        
+        let favouriteMovie = prof[0].favoriteMovieID;
+      
+        return movies[favouriteMovie].name;
+        
+        
+    }
+  render() {
+     return (
+      <div>
+        <header className="App-header">
+         
+          <h1 className="App-title">ReactND - Coding Practice</h1>
+        </header>
+        <h2>Favorite Movies</h2>
+		<ol>
+          {Object.values(users).map(user=>(
+            <li key={user.id}>
+             {user.name}'s favourite movie is {this.getFavouriteMove(user.id)}
+            </li>
+            ))}
+         </ol>
+        
+        
+        
+      </div>
+    );
+  }
+}
+
+export default App;
